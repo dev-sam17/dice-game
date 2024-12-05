2bf6a988b11dbb9a4de4c326d31a96becbfe3715
Saurav Kumar <codewizard.saurav@gmail.com>
2024-12-05 17:15:37 +0530

    made pages responsive

--- src/routes/createRoom/createRoom.component.jsx
+++ src/routes/createRoom/createRoom.component.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import { useState } from "react";
 import { useNavigate } from "react-router";
 import "./createRoom.scss"; // CSS file for styling
 
 export const CreateRoom = () => {
   const navigate = useNavigate();
+  const [isClicked, setIsClicked] = useState(false);
+  const [loader, setLoader] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(true);
+    setLoader(true);
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  };
 
   return (
     <>
@@ -11,33 +21,40 @@ export const CreateRoom = () => {
         <div className="selectRounds">
           <h1>Select Number of Rounds:</h1>
           <div className="numberOfRounds">
-            <button className="">1 Round</button>
-            <button className="">3 Rounds</button>
-            <button className="">5 Rounds</button>
+            <button onClick={handleClick}>1 Round</button>
+            <button onClick={handleClick}>3 Rounds</button>
+            <button onClick={handleClick}>5 Rounds</button>
           </div>
         </div>
-        <div className="showRoomId">
-          <div className="roomId">
-            Room Id: 684136463
-            <a className="copyButton">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                width="20"
-                height="20"
-                stroke-width="2"
-              >
-                <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z"></path>
-                <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1"></path>
-              </svg>
-            </a>
+        {isClicked && loader && (
+          <div className="showRoomId">
+            <div className="loader"></div>
           </div>
-          <button>Join Room</button>
-        </div>
+        )}
+        {isClicked && !loader && (
+          <div className="showRoomId">
+            <div className="roomId">
+              Room Id: 684136463
+              <a className="copyButton">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  width="20"
+                  height="20"
+                  stroke-width="2"
+                >
+                  <path d="M7 7m0 2.667a2.667 2.667 0 0 1 2.667 -2.667h8.666a2.667 2.667 0 0 1 2.667 2.667v8.666a2.667 2.667 0 0 1 -2.667 2.667h-8.666a2.667 2.667 0 0 1 -2.667 -2.667z"></path>
+                  <path d="M4.012 16.737a2.005 2.005 0 0 1 -1.012 -1.737v-10c0 -1.1 .9 -2 2 -2h10c.75 0 1.158 .385 1.5 1"></path>
+                </svg>
+              </a>
+            </div>
+            <button onClick={() => navigate("/game")}>Join Room</button>
+          </div>
+        )}
       </div>
       <button className="back-button" onClick={() => navigate(-1)}>
         Back
--- src/routes/joinRoom/joinRoom.component.jsx
+++ src/routes/joinRoom/joinRoom.component.jsx
@@ -1,46 +1,69 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router";
 import "./joinRoom.scss"; // CSS file for styling
 
 export const JoinRoom = () => {
   const navigate = useNavigate();
+  const [loader, setLoader] = useState(true);
+  const [inputId, setInputId] = useState("");
+
+  const handleJoinRoom = () => {
+    if (!!inputId) {
+      navigate("/game");
+    } else {
+      alert("Enter Room Id");
+    }
+  };
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  }, []);
 
   return (
     <>
       <div className="bg">
         <div className="enter-roomId">
           <h1>Enter Room Id</h1>
-          <input type="text" />
-          <button onClick={() => navigate("/game")}>Enter</button>
+          <input type="text" onChange={(e) => setInputId(e.target.value)} />
+          <button onClick={handleJoinRoom}>Enter</button>
         </div>
         <div className="available-rooms">
           <h2>Rooms Available:</h2>
-          <div className="roomList">
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
+          {loader && (
+            <div className="roomList">
+              <div className="loader"></div>
             </div>
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
+          )}
+          {!loader && (
+            <div className="roomList">
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
+              <div className="roomInfo">
+                Room 567326723 <span>Rounds: 3</span>
+                <button>Join</button>
+              </div>
             </div>
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
-            </div>
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
-            </div>
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
-            </div>
-            <div className="roomInfo">
-              Room 567326723 <span>Rounds: 3</span>
-              <button>Join</button>
-            </div>
-          </div>
+          )}
         </div>
       </div>
       <button className="back-button" onClick={() => navigate(-1)}>
